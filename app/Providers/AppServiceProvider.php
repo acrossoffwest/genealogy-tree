7f4d370e4deb2cedfb20a56682f928ec7eccd88b
Iurii Karpov <acrossoffwest@gmail.com>
2020-05-05 16:05:51 +0200

    (fix)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         if (config('app.secure')) {
-            URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
     }
 
